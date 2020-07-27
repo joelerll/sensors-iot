e0b3d009049d542db443431c081d0ccd9964661f
Joel Rodriguez <joelerll@gmail.com>
2020-07-27 16:27:56 -0500

    add login

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
 # Generic Iot
 
-
 # TODO
 
-- [ ] Setup Folder structure backend
+- [x] Setup Folder structure backend
 - [ ] Folder structure frontend
+- [ ] Login front
+- [ ] Login database
+- [ ] Login backend con mysql, api y endpoint
 - [ ] Docker file nodejs, mongodb, redis, mysql
 - [ ] Database schema Mysql
 - [ ] Login Admin
-- [ ] 
-
+- [ ]
 
 https://data.cityofnewyork.us/browse?sortBy=most_accessed&tags=map&utf8=%E2%9C%93&page=2
 https://data.world/dublin-city/8bb757a5-609e-4769-9bc6-b1e2667da2cd
 
-
 https://towardsdatascience.com/top-10-great-sites-with-free-data-sets-581ac8f6334
-
 
 https://www.kaggle.com/gireeshs/household-monthly-electricity-bill
 
---
+++ client/src/views/Login/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="Login" class="center-both">
+    <v-container fill-height fluid>
+      <v-card class="mx-auto" max-width="900px" min-width="700px" outlined>
+        <v-card-title class="headline">Login</v-card-title>
+        <v-list-item three-line>
+          <v-row no-gutters>
+            <v-col class="col-2">
+              <v-icon class="center-both">mdi-account</v-icon>
+            </v-col>
+            <v-col class="col-10">
+              <v-text-field class="pa-2" label="Usuario" required @keyup.enter="login"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-list-item>
+        <v-list-item three-line>
+          <v-row no-gutters>
+            <v-col class="col-2">
+              <v-icon class="center-both">mdi-textbox-password</v-icon>
+            </v-col>
+            <v-col class="col-10">
+              <v-text-field
+                type="password"
+                class="pa-2"
+                label="Contraseña"
+                required
+                @keyup.enter="login"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-list-item>
+        <v-card-actions>
+          <v-btn class="mx-auto" x-large color="primary" @click="login" @keyup.enter="login">Entrar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  methods: {
+    login() {
+      console.log("login");
+    },
+  },
+};
+</script>
+
+<style lang="css">
+.center-both {
+  width: 100%;
+  height: 100%;
+}
+</style>
